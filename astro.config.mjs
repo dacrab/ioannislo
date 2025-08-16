7bd67f3cad43d4acf70faf87c11594518f72254b
Vaggelis kavouras <vaggeliskavouras6@gmail.com>
2025-08-16 22:02:42 +0300

    Remove Tailwind integration and update dependencies
    - Eliminate Tailwind CSS integration from astro.config.mjs and remove related configurations.
    - Update package.json to reflect the latest version of Astro and @types/node.
    - Delete unused PostCSS and Tailwind configuration files to streamline the project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    })
-  ],
   build: {
     inlineStylesheets: 'auto'
   },
